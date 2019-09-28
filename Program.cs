fb4b633cb79b8786a7bc129347bb55e2954d95ff
Denny Petersson <petersson.denny@gmail.com>
2019-09-28 12:41:18 +0200

    creating project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Workshop_2___Design
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
