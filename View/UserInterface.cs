7d49f7294025ce112a3fdfefc0402ab72a418f48
Denny Petersson <petersson.denny@gmail.com>
2019-09-30 22:10:02 +0200

    adding menu and option to delete member

--- View/UserInterface.cs
+++ View/UserInterface.cs
@@ -4,6 +4,15 @@ namespace View
 {
   public class UserInterface
   {
+    public void PresentMenu()
+    {
+      Console.WriteLine("Please choose one below.");
+      Console.WriteLine("1. Register Member");
+      Console.WriteLine("2. Delete Member");
+      Console.WriteLine("Q to quit");
+      Console.Write("Enter choice: ");
+    }
+
     public void PresentMemberCreationUI() => Console.WriteLine("Please create a new member below.");
 
     public void PresentMemberNameUI() => Console.Write("Name: ");
@@ -11,5 +20,11 @@ namespace View
     public void PresentMemberPersonalIdUI() => Console.Write("Personal id: ");
 
     public string CollectData() => Console.ReadLine();
+
+    public void PresentDeleteMemberUI()
+    {
+      Console.WriteLine("Please enter id of member that should be deleted.");
+      Console.Write("Member ID: ");
+    }
   }
 }
